783aacf14b59f23455d174f9640b5c680a13375e
Julia.Tandetzki <julia.tandetzki@thuenen.de>
2023-11-13 15:41:23 +0100

    test python files to see connection to git

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+categories = ['categ 1', 'categ  2', 'categ  3', 'categ  4']
+values = [3, 7, 2, 5]
+
+plt.bar(categories, values, color='blue')
+
+plt.xlabel('categ ')
+plt.ylabel('values')
+plt.title('Test Figure')
+
+plt.show()
