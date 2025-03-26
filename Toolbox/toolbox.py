c83e3d2a1336e679e23e01d91b28857c3b810b44
Christian Morland <christian.morland@thuenen.de>
2025-03-26 09:32:01 +0100

    add scenario_folder_path as option to toolbox.py

--- Toolbox/toolbox.py
+++ Toolbox/toolbox.py
@@ -1,14 +1,19 @@
 from Toolbox.classes.import_data import import_pkl_data
 from Toolbox.classes.dashboard import DashboardPlotter
+from pathlib import Path
+PACKAGEDIR = Path(__file__).resolve().parent.parent
 
 class timba_dashboard:
-    def __init__(self,num_files_to_read:int=10):
+    def __init__(self,num_files_to_read:int=10,
+                 scenario_folder_path:Path=PACKAGEDIR / Path("Input\\Scenario_Files")):
         self.num_files_to_read = num_files_to_read
+        self.scenario_folder_path = scenario_folder_path
 
     def import_data(self):
         import warnings
         warnings.simplefilter(action='ignore', category=FutureWarning)
-        import_pkl = import_pkl_data(num_files_to_read=self.num_files_to_read)
+        import_pkl = import_pkl_data(num_files_to_read=self.num_files_to_read,
+                                     SCENARIOPATH=self.scenario_folder_path)
         self.data = import_pkl.combined_data()
 
     def call_dashboard(self):
@@ -19,6 +24,7 @@ class timba_dashboard:
         self.call_dashboard() 
 
 if __name__ == "__main__":
-    td = timba_dashboard(num_files_to_read=4)
+    td = timba_dashboard(num_files_to_read=4,
+                         scenario_folder_path = PACKAGEDIR / Path("Input\\Scenario_Files"))
     td.run()
 
