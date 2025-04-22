54d4e6df3ddddff24000a2518c1331ff1d114111
Christian.Morland <christian.morland@thuenen.de>
2025-04-22 16:23:00 +0200

    add three more dashboards as dummies for future applications

---
+++ Toolbox/classes/bilateral_trade_db.py
@@ -0,0 +1,212 @@
+import plotly.graph_objects as go
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import webbrowser
+from threading import Timer
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import plotly.express as px
+import textwrap
+
+PACKAGEDIR = Path(__file__).parent.parent.absolute()
+
+class BT_DashboardPlotter:
+
+    def __init__(self, data):
+        self.data = data
+        self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+        self.start = self.data['year'].min()
+        self.end = self.data['year'].max()
+        self.color_list = [
+            '#2A4D69',  # Dunkelblau
+            '#4B8BBE',  # Hellblau
+            '#D35400',  # Dunkelorange
+            '#AAB7B8',  # Grau
+            '#9B59B6',  # Lila
+            '#2980B9',  # Blau
+            '#27AE60',  # Grün
+            '#6C757D',  # Dunkelgrau
+            '#F1C40F',  # Senfgelb
+            '#E67E22',  # Orange  
+        ]
+        self.logo = PACKAGEDIR/'timba_dashboard_logo.png'
+        self.create_layout()
+        self.create_callbacks()
+
+    def create_layout(self):
+        dropdown_style = {'height': '30px','marginBottom': '10px'}
+        self.app.layout = dbc.Container([
+            dbc.Row([
+                dbc.Col(width=5),
+                dbc.Col(
+                    html.Img(src=self.app.get_asset_url('timba_dashboard_logo.png'), style={'height': '90px', 'width': 'auto'}),
+                    width=1
+                ),
+                dbc.Col()
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.H4("Filters", className="card-title"),
+                            dcc.Dropdown(id='region-dropdown',
+                                         options=[{'label': i, 'value': i} for i in sorted(self.data['ISO3'].dropna().unique())],
+                                         multi=True,
+                                         placeholder="Select Country...",
+                                         style=dropdown_style),
+                            dcc.Dropdown(id='continent-dropdown',
+                                         options=[{'label': i, 'value': i} for i in sorted(self.data['Continent'].dropna().unique())],
+                                         placeholder="Select Continent...",
+                                         multi=True,
+                                         style=dropdown_style),
+                            dcc.Dropdown(id='domain-dropdown',
+                                         options=[{'label': i, 'value': i} for i in sorted(self.data['domain'].dropna().unique())],
+                                         placeholder="Select Domain...",
+                                         multi=True,
+                                         style=dropdown_style),
+                            dcc.Dropdown(id='commodity-dropdown',
+                                         options=[{'label': i, 'value': i} for i in sorted(self.data['Commodity'].dropna().unique())],
+                                         placeholder="Select Commodity...",
+                                         multi=True,
+                                         style=dropdown_style),
+                            dcc.Dropdown(id='commodity-group-dropdown',
+                                         options=[{'label': i, 'value': i} for i in self.data['Commodity_Group'].dropna().unique().tolist()],
+                                         placeholder="Select Commodity Group...",
+                                         multi=True,
+                                         style=dropdown_style),
+                            dcc.Dropdown(id='scenario-filter',
+                                         options=[{'label': i, 'value': i} for i in self.data['Scenario'].unique()],
+                                         placeholder="Select Scenario...",
+                                         multi=True,
+                                         style=dropdown_style),
+                            html.Button("Download CSV", id="btn_csv"),
+                            dcc.Download(id="download-dataframe-csv"),
+                        ])
+                    ], className="mb-4", style={'white': 'white'}),
+                    dbc.Card([
+                        dbc.CardBody([
+                                dcc.Graph(style={'height': '45vh','width':'46vh'})
+                    ])
+                ], style={'white': 'white'})  #price box
+            ], width=3),
+            dbc.Col([
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.CardBody([
+                                dcc.Graph(style={'height': '84.5vh'})
+                            ])
+                        ], style={'backgroundColor': 'white'}) 
+                    ], width=8),
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.CardBody([
+                                dcc.Graph(style={'height': '39vh'})
+                            ])
+                        ], style={'backgroundColor': 'white', 'marginBottom': '20px'}), # Abstand hinzugefügt
+                        dbc.Card([
+                            dbc.CardBody([
+                                dcc.Dropdown(),
+                                 dcc.Graph(style={'height': '31.75vh'})
+                            ])
+                        ], style={'backgroundColor': 'white'})
+                    ], width=4),
+                ]),
+            ], width=9)
+        ])
+    ], fluid=True, style={'backgroundColor': 'white'}) #gesamthintergrund
+
+    def create_callbacks(self):
+        @self.app.callback(
+            [Output('quantity-plot', 'figure'),
+             Output('price-plot', 'figure'),
+             Output('forstock-plot', 'figure')],
+            [Input('region-dropdown', 'value'),
+             Input('continent-dropdown', 'value'),
+             Input('domain-dropdown', 'value'),
+             Input('commodity-dropdown', 'value'),
+             Input('commodity-group-dropdown', 'value'),
+             Input('scenario-filter', 'value')]
+        )
+        def update_plots(region, continent, domain, commodity, commodity_group,scenario):
+            return self.update_plot_data(region, continent, domain, commodity, commodity_group,scenario)
+
+        @self.app.callback(
+            Output('world-map', 'figure'),
+            [Input('scenario-filter', 'value'),
+             Input('year-filter', 'value'),
+             Input('region-dropdown', 'value'),
+             Input('continent-dropdown', 'value'),
+             Input('domain-dropdown', 'value'),
+             Input('commodity-dropdown', 'value'),
+             Input('commodity-group-dropdown', 'value')]
+        )
+        def update_world_map(scenario, year, region, continent, domain, commodity, commodity_group):
+            return self.create_world_map(region, continent, domain, commodity, commodity_group, scenario, year)
+
+        @self.app.callback(
+            Output("download-dataframe-csv", "data"),
+            Input("btn_csv", "n_clicks"),
+            [State('region-dropdown', 'value'),
+            State('continent-dropdown', 'value'),
+            State('domain-dropdown', 'value'),
+            State('commodity-dropdown', 'value'),
+            State('commodity-group-dropdown', 'value'),
+            State('scenario-filter', 'value')],  # Geändert von Input zu State
+            prevent_initial_call=True
+        )
+        def func(n_clicks, region, continent, domain, commodity, commodity_group, scenario):
+            if n_clicks is None:
+                raise dash.exceptions.PreventUpdate
+            filtered_data = self.filter_data(region, continent, domain, commodity, commodity_group, scenario)
+            return dcc.send_data_frame(filtered_data.to_csv, "filtered_data.csv")
+
+
+    def filter_data(self, region, continent, domain, commodity, commodity_group, scenario):
+        filtered_data = self.data
+        if region and isinstance(region, list):
+            filtered_data = filtered_data[filtered_data['ISO3'].isin(region)]
+        if continent and isinstance(continent, list):
+            filtered_data = filtered_data[filtered_data['Continent'].isin(continent)]
+        if domain and isinstance(domain, list):
+            filtered_data = filtered_data[filtered_data['domain'].isin(domain)]
+        if commodity and isinstance(commodity, list):
+            filtered_data = filtered_data[filtered_data['Commodity'].isin(commodity)]
+        if commodity_group and isinstance(commodity_group, list):
+            filtered_data = filtered_data[filtered_data['Commodity_Group'].isin(commodity_group)]
+        if scenario and isinstance(scenario, list):
+            filtered_data = filtered_data[filtered_data['Scenario'].isin(scenario)]
+        filtered_data = self.remove_extreme_outliers(df=filtered_data, col='price')
+        return filtered_data
+
+    def update_plot_data(self, region, continent, domain, commodity, commodity_group, scenario):
+        pass
+
+    def generate_title(self, region, continent, domain, commodity, commodity_group):
+        title_parts = []
+        if region:
+            title_parts.append(f"{region}")
+        if continent:
+            title_parts.append(f"{continent}")
+        if domain:
+            title_parts.append(f"{domain}")
+        if commodity:
+            title_parts.append(f"{commodity}")
+        if commodity_group:
+            title_parts.append(f"{commodity_group}")
+        title = ", ".join(title_parts) if title_parts else "all data"
+        clean_title = title.replace("'", "").replace("[", "").replace("]", "")
+        return clean_title
+
+    def create_world_map(self, region, continent, domain, commodity, commodity_group, scenario=None, year=None):
+        pass
+   
+    def open_browser(self):
+        webbrowser.open_new("http://localhost:8050")
+
+    def run(self):
+        Timer(1, self.open_browser).start()
+        self.app.run(host='localhost', debug=False, dev_tools_ui=False, dev_tools_hot_reload=False, port=8050)
--- Toolbox/cli/cli.py
+++ Toolbox/cli/cli.py
@@ -1,22 +1,48 @@
 import click
 import warnings
-from Toolbox.toolbox import timba_dashboard
+from Toolbox.toolbox import timba_dashboard, bilateral_trade_dashboard
 import Toolbox.parameters.paths as toolbox_paths
 from pathlib import Path
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+#Dashboard Command
 @click.command()
 @click.option('-NF', '--num_files', default=10, 
               show_default=True, required=True, type=int, 
-              help="Specify the number of most recent .pkl files to read. Limits output to prevent overcrowding.")
+              help="Number of .pkl files to read")
 @click.option('-FP', '--sc_folderpath', default=toolbox_paths.SCINPUTPATH, 
               show_default=True, required=True, type=Path, 
-              help="Define the folder where the code will look for .pkl files containing the scenarios.")
-
-def cli(num_files,sc_folderpath):    
-    td = timba_dashboard(num_files_to_read=num_files,
-                         scenario_folder_path=sc_folderpath)
+              help="Folder path for scenarios")
+def dashboard_cli(num_files, sc_folderpath):    
+    click.echo("Begin to show default dashboard")
+    td = timba_dashboard(
+        num_files_to_read=num_files,
+        scenario_folder_path=sc_folderpath
+    )
     td.run()
 
-if __name__ == '__main__':
-    cli()
+#Validation Command
+@click.command()
+def validation_cli():    
+    click.echo("Validation is started")
+
+#bilateral trade Command
+@click.option('-NF', '--num_files', default=10, 
+              show_default=True, required=True, type=int, 
+              help="Number of .pkl files to read")
+@click.option('-FP', '--sc_folderpath', default=toolbox_paths.SCINPUTPATH, 
+              show_default=True, required=True, type=Path, 
+              help="Folder path for scenarios")
+@click.command()
+def bilateral_trade_cli(num_files, sc_folderpath):    
+    click.echo("Bilateral Trade dashboard is started")
+    btdb = bilateral_trade_dashboard(
+        num_files_to_read=num_files,
+        scenario_folder_path=sc_folderpath
+    )
+    btdb.run()
+
+#scenario comparision Command
+@click.command()
+def scenario_comparision_cli():    
+    click.echo("Scenario comparion is started")
